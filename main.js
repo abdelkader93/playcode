cb57da2519fe9fc29bfcbfbf179249d1e3da0cf0
Abdelkader.ferchichi <abdelkader.ferchichi@esprit.tn>
2024-09-02 11:52:39 +0100

    checkpoint playcode

---
+++ main.js
@@ -0,0 +1,55 @@
+//String Manipulation Functions
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function countCharacters(str) {
+    return str.length;
+}
+
+function capitalizeWords(sentence) {
+    return sentence.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+//Array Functions
+
+function findMax(arr) {
+    return Math.max(...arr);
+}
+
+function findMin(arr) {
+    return Math.min(...arr);
+}
+
+function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+
+function filterArray(arr, condition) {
+    return arr.filter(condition);
+}
+
+//Mathematical Functions
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+function fibonacci(n) {
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
